6864fe61ab1c7c704bbee55a74ec9cff5baa4ff7
olybol <71195708+ArnolddeBolster@users.noreply.github.com>
2026-03-26 16:08:43 +0100

    Add constants for DAB Net Receiver configuration

---
+++ const.py
@@ -0,0 +1,10 @@
+DOMAIN = "dabnet"
+
+CONF_HOST = "host"
+CONF_NAME = "name"
+CONF_POLL_INTERVAL = "poll_interval"
+
+DEFAULT_NAME = "DAB Net Receiver"
+DEFAULT_POLL_INTERVAL = 10  # seconden
+
+PLATFORMS = ["media_player"]
